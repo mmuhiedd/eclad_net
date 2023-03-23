Fix data normalization in TVM
Use the torch dataloader used to test model in order to get the exact same input format

TVM/TVMdefinitions.py
@@ -1,42 +1,21 @@
-from PIL import Image
 import os
 import numpy as np
+from torch.utils.data import DataLoader
+import sys
+sys.path.append("{cur_dir}/../pytorchModels/".format(cur_dir=os.path.dirname(os.path.abspath(__file__))))
+from definitions import *
+from classes import *
+
 
 def load_batch_image(batch_size=32):
-    '''
-    Load 'batch_size' images and corresponding labels from the test set
-    '''
-    src = './dataset/test'
-
-    half_batch_three = batch_size//2
-    
-    img_array = np.zeros(shape=(batch_size, 3, 32, 32))
-    class_array = np.zeros(shape=(batch_size))
-    i = 0
-
-    for dirpath, dirs,_ in os.walk(src):
-        for d in dirs:
-            dir_str = os.path.join(dirpath, d)
-            
-            # define class hot encoding
-            if d =="res": 
-                class_ohe = 0
-            else:
-                class_ohe = 1
-
-            for _, _, files in os.walk(dir_str):
-                for j, file in enumerate(files):
-                    temp_img_str = os.path.join(dir_str, file)
-                    resized_image = Image.open(temp_img_str).resize((32,32))
-
-                    # Our input image is in HWC layout while ONNX expects CHW input, so convert the array
-                    resized_image = np.transpose(resized_image, (2, 0, 1))
-
-                    img_array[i*half_batch_three + j] = np.array(resized_image).astype("float32")
-                    class_array[i*half_batch_three + j] = class_ohe
-
-                    if j == half_batch_three-1:
-                        i += 1 
-                        break
-
-    return img_array, class_array
+    img_list_test, class_list_test_t = read_images(train=False)
+    class_list_test_t = onehotencoding_class(class_list_test_t)
+    transformed_dataset_test = Logo_Dataset(img_list_test, class_list_test_t,
+                                         transform = transforms.Compose([Rescale(32), ToTensor()]))
+    testloader = DataLoader(transformed_dataset_test, batch_size=batch_size,
+                        shuffle=True, pin_memory=True)
+    # Retrieve only first batch
+    for _,batch in enumerate(testloader):
+        images_batch = batch["image"].numpy()
+        labels_batch = batch["class_name"].numpy()
+        return images_batch,labels_batch