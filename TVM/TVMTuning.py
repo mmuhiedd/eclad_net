e1ec001413825794189a8d5fa222040cee523ac9
mmuhiedd <marinmuhieddine17@gmail.com>
2023-03-23 14:14:28 -0400

    Add metrics mesurement and tuning for TVM

--- TVM/TVMTuning.py
+++ TVM/TVMTuning.py
@@ -1,12 +1,15 @@
 import sys
 from tvm.driver import tvmc
 import tvm
+import tvm.auto_scheduler as auto_scheduler
+from tvm.autotvm.tuner import XGBTuner
+from tvm import autotvm
 import argparse
 import os
 import onnx
 from tvm.contrib import graph_executor
 import numpy as np
-from TVMdefinitions import load_batch_image
+from TVMdefinitions import * 
 
 ##############
 ## ARGS PARSING 
@@ -43,7 +46,6 @@ with tvm.transform.PassContext(opt_level=3):
 
 
 device = tvm.device(str(target), 0)
-print(lib["default"](device))
 
 module = graph_executor.GraphModule(lib["default"](device))
 
@@ -54,3 +56,71 @@ module.set_input(input_name, images_arr)
 module.run()
 output_shape = (32, 2)
 tvm_output = module.get_output(0, tvm.nd.empty(output_shape)).numpy()
+
+
+""" 
+PERFORMANCES
+ """
+
+## Exec times 
+metrics = compute_inference_time(module)
+# time in ms
+print(metrics)
+
+
+
+## Accuracy score
+compute_acc(tvm_output,labels_arr)
+
+
+
+'''
+Tuning !! 
+'''
+# tuning path path
+tuning_path = "TVM/tuning/"
+if not(os.path.exists(tuning_path)):
+    os.makedirs(tuning_path)
+tuning_file =os.path.join(tuning_path,"ghostEclad_{}_{}_tuning.json".format(args.ratio1,args.ratio2)) if isGhostNet else os.path.join(onnx_path,"ecladNet_tuning.json")
+
+
+number = 10
+repeat = 1
+min_repeat_ms = 0  # since we're tuning on a CPU, can be set to 0
+timeout = 10  # in seconds
+
+# create a TVM runner
+runner = autotvm.LocalRunner(
+    number=number,
+    repeat=repeat,
+    timeout=timeout,
+    min_repeat_ms=min_repeat_ms,
+    enable_cpu_cache_flush=True,
+)
+
+tuning_option = {
+    "tuner": "xgb",
+    "trials": 20,
+    "early_stopping": 100,
+    "measure_option": autotvm.measure_option(
+        builder=autotvm.LocalBuilder(build_func="default"), runner=runner
+    ),
+    "tuning_records": tuning_file,
+}
+
+# begin by extracting the tasks from the onnx model
+tasks = autotvm.task.extract_from_program(mod["main"], target=target, params=params)
+
+# Tune the extracted tasks sequentially.
+for i, task in enumerate(tasks):
+    prefix = "[Task %2d/%2d] " % (i + 1, len(tasks))
+    tuner_obj = XGBTuner(task, loss_type="rank")
+    tuner_obj.tune(
+        n_trial=min(tuning_option["trials"], len(task.config_space)),
+        early_stopping=tuning_option["early_stopping"],
+        measure_option=tuning_option["measure_option"],
+        callbacks=[
+            autotvm.callback.progress_bar(tuning_option["trials"], prefix=prefix),
+            autotvm.callback.log_to_file(tuning_option["tuning_records"]),
+        ],
+    )
--- TVM/TVMdefinitions.py
+++ TVM/TVMdefinitions.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append("{cur_dir}/../pytorchModels/".format(cur_dir=os.path.dirname(os.path.abspath(__file__))))
 from definitions import *
 from classes import *
+import timeit
 
 
 def load_batch_image(batch_size=32):
@@ -19,3 +20,37 @@ def load_batch_image(batch_size=32):
         images_batch = batch["image"].numpy()
         labels_batch = batch["class_name"].numpy()
         return images_batch,labels_batch
+    
+
+def compute_inference_time(module):
+    '''
+    By default it's only on one batch
+    '''
+
+    timing_number = 10
+    timing_repeat = 10
+    metrics = (
+        np.array(timeit.Timer(lambda: module.run()).repeat(repeat=timing_repeat, number=timing_number))
+        * 1000
+        / timing_number
+    )
+    metrics = {
+        "mean": np.mean(metrics),
+        "median": np.median(metrics),
+        "std": np.std(metrics),
+    }
+
+    return metrics
+
+def compute_acc(preds,labels):
+    correct,total = 0.0,0.0
+
+    for cur_pred,label in zip(preds,labels):
+        cur_max_pred_id = np.argmax(cur_pred)
+        if cur_max_pred_id == label:
+            correct += 1
+        total +=1
+            
+
+    acc = correct/total
+    print("Accuracy : {}  ({}/{})".format(acc,correct,total))
